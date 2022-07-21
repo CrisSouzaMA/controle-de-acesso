ad2c38f77f7465e21878294752ebd06c49c9bb07
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-07-21 11:30:28 -0300

    Feat: implementa classe principal com lógica para interação com usuário do sistema

--- Controle-de-acesso/src/main/java/controledeacesso/Principal.java
+++ Controle-de-acesso/src/main/java/controledeacesso/Principal.java
@@ -1,4 +1,36 @@
 package main.java.controledeacesso;
 
+import java.util.Scanner;
+
 public class Principal {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Relatorio relatorio = new Relatorio();
+
+    short opcaoEscolhidaConvertida = 1;
+
+    while (opcaoEscolhidaConvertida != 2) {
+      System.out.println("Entre com o número correspondente à opção desejada:");
+      System.out.println("1 - Acessar o estabelecimento");
+      System.out.println("2 - Finalizar sistema e mostrar relatório");
+
+      String opcaoEscolhida = scanner.next();
+      opcaoEscolhidaConvertida = Short.parseShort(opcaoEscolhida);
+
+      if (opcaoEscolhidaConvertida == 1) {
+        System.out.println("Entre com a sua idade:");
+        String idade = scanner.next();
+        int idadeConvertida = Integer.parseInt(idade);
+        relatorio.addArrayIdades(idadeConvertida);
+      } else if (opcaoEscolhidaConvertida == 2) {
+        relatorio.relatorioEncerrado();
+      } else {
+        System.out.println("Entre com uma opção válida!");
+      }
+    }
+
+    scanner.close();
+  }
 }
+
